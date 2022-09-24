fdfaaf90f95bf27abbd3fd7d5771e3cef46efa9f
Erasmo Hernández <erasmoh@gmail.com>
2022-09-23 21:06:16 -0300

    Feat: add players in bulk

--- src/components/PlayersForm.jsx
+++ src/components/PlayersForm.jsx
@@ -3,111 +3,160 @@ import PlayersList from "./PlayersList";
 import TeamList from "./TeamList";
 
 function PlayersForm() {
-    const [players, setPlayers] = useState([]);
-    const [newPlayer, setNewPlayer] = useState("");
-    const [teams, setTeams] = useState([]);
-    const [nop, setNop] = useState(1);
+  const [players, setPlayers] = useState([]);
+  const [newPlayer, setNewPlayer] = useState("");
+  const [teams, setTeams] = useState([]);
+  const [nop, setNop] = useState(1);
+  const [multiline, setMultiline] = useState(false);
+  const [multilist, setMultilist] = useState("");
 
-    const handleChange = ({ target }) => {
-        setNewPlayer(target.value);
-    };
-    const handlePlayers = ({ target }) => {
-        setNop(target.value);
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setPlayers((prev) => {
-            return [...prev, newPlayer];
-        });
-        // clears new player field
-        setNewPlayer("");
-    };
-    const randomPlayers = (e) => {
-        e.preventDefault();
-        const listOfPlayers = [...players];
-        const virtualTeams = [];
+  const handleMultilistChange = ({ target }) => {
+    const users = target.value;
+    setMultilist(users);
+  };
 
-        const randomTeams = [...listOfPlayers].sort(() => Math.random() - 0.5);
-        while (randomTeams.length >= nop) {
-            const newTeam = randomTeams.splice(0, nop);
-            virtualTeams.push(newTeam);
-        }
-        setTeams((teams) => virtualTeams);
-    };
-    const resetPlayers = (e) => {
-        setPlayers([]);
-        setTeams([]);
-        setNewPlayer("");
-        setNop(1);
-    };
-    const handleDelete = (player) => {
-        const temp = [...players];
-        temp.splice(player, 1);
-        setPlayers(temp);
-    };
+  const handleMultiline = () => {
+    setMultiline(!multiline);
+  };
+  const handleChange = ({ target }) => {
+    setNewPlayer(target.value);
+  };
+  const handlePlayers = ({ target }) => {
+    setNop(target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const playerValues = multiline ? multilist.split("\n") : newPlayer;
+    const newList = [];
+    const finalList = newList.concat(players, playerValues);
+    setPlayers(finalList);
 
-    return (
-        <div className="row">
-            <div className="col-6">
-                <form onSubmit={handleSubmit}>
-                    <div className="form-row">
-                        <div className="form-group col-md-12">
-                            <label htmlFor="nOfPlayers">
-                                Number of Players
-                            </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                value={nop}
-                                onChange={handlePlayers}
-                            />
-                            <label htmlFor="playerName">Player Name</label>
-                            <input
-                                onChange={handleChange}
-                                type="text"
-                                className="form-control"
-                                id="player"
-                                placeholder="Player Name"
-                                name="newPlayer"
-                                value={newPlayer}
-                            />
-                        </div>
-                        <button
-                            disabled={newPlayer === ""}
-                            type="submit"
-                            className="btn btn-primary btn-sm my-2"
-                        >
-                            Add Player
-                        </button>
-                    </div>
-                </form>
+    console.log(playerValues);
+    console.log("players", players);
+    // clears new player field
+    !multiline && setNewPlayer("");
+  };
+  const randomPlayers = (e) => {
+    e.preventDefault();
+    const listOfPlayers = [...players];
+    const virtualTeams = [];
+
+    const randomTeams = [...listOfPlayers].sort(() => Math.random() - 0.5);
+    while (randomTeams.length >= nop) {
+      const newTeam = randomTeams.splice(0, nop);
+      virtualTeams.push(newTeam);
+    }
+    setTeams((teams) => virtualTeams);
+  };
+  const resetPlayers = (e) => {
+    setPlayers([]);
+    setTeams([]);
+    setNewPlayer("");
+    setNop(1);
+  };
+  const handleDelete = (player) => {
+    const temp = [...players];
+    temp.splice(player, 1);
+    setPlayers(temp);
+  };
+
+  return (
+    <div className="row">
+      <div className="col-6">
+        <form onSubmit={handleSubmit}>
+          <div className="form-row">
+            <div className="form-group col-md-12">
+              <div className="row">
+                <div className="col-md-4 d-flex align-items-center justify-content-between">
+                  <input
+                    type="radio"
+                    id="mode"
+                    name="mode"
+                    value="single"
+                    onChange={handleMultiline}
+                    checked={!multiline}
+                  />
+                  <label htmlFor="single">Single add</label>
+                  <input
+                    type="radio"
+                    id="mode"
+                    name="mode"
+                    value="multi"
+                    onChange={handleMultiline}
+                    checked={multiline}
+                  />
+                  <label htmlFor="multi">Bulk add</label>
+                </div>
+                <div className="col-md-8">
+                  <label htmlFor="nOfPlayers">Players per team</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={nop}
+                    onChange={handlePlayers}
+                  />
+                </div>
+              </div>
+              {multiline ? (
+                <>
+                  <label htmlFor="playerName">Player Names</label>
+                  <textarea
+                    className="form-control"
+                    value={multilist}
+                    onChange={handleMultilistChange}
+                    rows={nop}
+                  ></textarea>
+                </>
+              ) : (
+                <>
+                  <label htmlFor="playerName">Player Name</label>
+                  <input
+                    onChange={handleChange}
+                    type="text"
+                    className="form-control"
+                    id="player"
+                    placeholder="Player Name"
+                    name="newPlayer"
+                    value={newPlayer}
+                  />
+                </>
+              )}
             </div>
-            <div className="col-6">
-                {players.length > 0 ? (
-                    <React.Fragment>
-                        <PlayersList
-                            players={players}
-                            onDelete={handleDelete}
-                        />
-                        <button
-                            disabled={players.length === 0}
-                            onClick={randomPlayers}
-                            className="btn btn-primary btn-sm mx-2"
-                        >
-                            Create teams
-                        </button>
-                        <button
-                            onClick={resetPlayers}
-                            className="btn btn-warning btn-sm"
-                        >
-                            Reset
-                        </button>
-                    </React.Fragment>
-                ) : 'Add some players'}
-            </div>
-            {teams.length > 0 ? <TeamList teams={teams} /> : "No teams yet"}
-        </div>
-    );
+            <button
+              disabled={
+                (!multiline && newPlayer === "") ||
+                (multiline && multilist === "")
+              }
+              type="submit"
+              className="btn btn-primary btn-sm my-2"
+            >
+              {multiline ? "Add Players" : "Add Player"}
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="col-6">
+        {players.length > 0 ? (
+          <>
+            <PlayersList players={players} onDelete={handleDelete} />
+            <button
+              disabled={players.length === 0}
+              onClick={randomPlayers}
+              className="btn btn-primary btn-sm mx-2"
+            >
+              Create teams
+            </button>
+            <button onClick={resetPlayers} className="btn btn-warning btn-sm">
+              Reset
+            </button>
+          </>
+        ) : (
+          "Add some players"
+        )}
+      </div>
+      {teams.length > 0 ? <TeamList teams={teams} /> : "No teams yet"}
+    </div>
+  );
 }
 
 export default PlayersForm;
